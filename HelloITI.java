6477a75687ad8b3c62617975ac8f4ba32bda9ad3
Hadir Elnagdy <hadirelnagdy@gmail.com>
2023-12-10 14:35:38 +0200

    Editing HelloITI

--- HelloITI.java
+++ HelloITI.java
@@ -1,11 +1,8 @@
 class HelloITI{
 public void printHelloITI{
-	System.out.println("Hello ITI!");
+	System.out.println("Hello ITI!!");
 }
 
-public void printHello{
-	System.out.println("Hello!");
-}
 
 
 }
